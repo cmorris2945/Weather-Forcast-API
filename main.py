4394b168c54c6bfaac7f266853276cb72f8669b2
C <30676606+cmorris2945@users.noreply.github.com>
2024-05-05 06:28:51 +0000

    modified main program and debugged

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+
+
+def get_weather(city, units='metrics', api_key= '99e07b5c10d550fd7f4b60a532026089'):
+  url = f"http://api.openweathermap.org/data/2.5/forecast?q={city}&appid= {api_key}&units{units}"
+  r = requests.get(url)
+  content = r.json()
+  return content
+
+print(get_weather(city='San Antonio'))
+ 
