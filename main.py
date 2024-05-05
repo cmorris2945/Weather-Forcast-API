b86a19cecad8c8460b646ff1c8e25bb2233ea805
C <30676606+cmorris2945@users.noreply.github.com>
2024-05-05 03:39:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import requests
 
 
-def get_weather(city, units='metrics', api_key= '99e07b5c10d550fd7f4b60a532026'):
-  url = f"http://api.openweathermap.org/data/2.5/forecast?q={city}&appid= {api_key}&units{units}"
-  r = requests.get(url)
-  content = r.json()
-  return content
+def get_weather(city,
+                    units='metric',
+                    api_key='99e07b5c10d550fd7f4b60a532026089'):
+                url = f"http://api.openweathermap.org/data/2.5/forecast?q={city}&appid= {api_key}&units{units}"
+                r = requests.get(url)
+                content = r.json()
+                with open('data.txt', 'a') as file:
+                                for dicty in content['list']:
+                                                file.write(
+                                                    f"{dicty['dt_txt']}, {dicty['main']['temp']}, {dicty['weather'][0]['description']} \n"
+                                                )
+
 
 print(get_weather(city='San Antonio'))
- 
